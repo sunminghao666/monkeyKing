90942845c42f2e748c5d17f56cf9bae29bd51adc
sunminghao666 <40458630+sunminghao666@users.noreply.github.com>
2019-04-07 22:18:45 +0800

    测试Git提交
    测试Git提交

--- src/main/java/com/monkey/bean/respBean/SearchPatientBean.java
+++ src/main/java/com/monkey/bean/respBean/SearchPatientBean.java
@@ -1,6 +1,10 @@
 package com.monkey.bean.respBean;
 
-public class SearchPatientBean {
+import java.io.Serializable;
+
+public class SearchPatientBean implements Serializable {
+
+	private static final long serialVersionUID = 4054073907289814834L;
 
 	private Long patientId;
 
