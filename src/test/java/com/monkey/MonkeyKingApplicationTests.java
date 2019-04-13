29ab138a073db68fbfe44e7cb0f81b10e6449d3a
sunminghao666 <40458630+sunminghao666@users.noreply.github.com>
2019-04-13 13:40:27 +0800

    213

--- src/test/java/com/monkey/MonkeyKingApplicationTests.java
+++ src/test/java/com/monkey/MonkeyKingApplicationTests.java
@@ -1,16 +1,31 @@
 package com.monkey;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.monkey.taf.common.Tools;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MonkeyKingApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
 
+	public static void main(String[] args) {
+		Date date1 = new Date();
+		date1.setHours(9);
+		date1.setMinutes(0);
+		date1.setSeconds(0);
+		try {
+			System.out.println(Tools.converToString(date1, Tools.FULLFORMAT));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
